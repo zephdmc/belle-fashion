68da3ca9db3f87d987ec9872237450734cc5f534
Ezeji Zephrinus <zephdmc@gmail.com>
2025-10-20 16:42:04 +0100

    Update HomePage.jsx
    aj

--- fashion-designing/cakestore-main/client/src/pages/home/HomePage.jsx
+++ fashion-designing/cakestore-main/client/src/pages/home/HomePage.jsx
@@ -50,7 +50,7 @@ const ImageSlideShow = ({ isMobile = false }) => {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 4000);
+    }, 400000);
 
     return () => clearInterval(interval);
   }, [images.length]);
