fc633ec9e9ee2eab4e4b86e5c722ccf03c3739eb
Ezeji Zephrinus <zephdmc@gmail.com>
2025-10-24 10:31:44 +0100

    Update CartPage.jsx
    dff

--- fashion-designing/cakestore-main/client/src/pages/cart/CartPage.jsx
+++ fashion-designing/cakestore-main/client/src/pages/cart/CartPage.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { motion, AnimatePresence } from 'framer-motion';
 import { useCart } from '../../context/CartContext';
