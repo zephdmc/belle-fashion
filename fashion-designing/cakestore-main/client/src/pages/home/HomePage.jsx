e78ff3bbf59875c9f624245fb2ac7f8cd141db90
Ezeji Zephrinus <zephdmc@gmail.com>
2025-10-24 13:59:46 +0100

    Update HomePage.jsx

--- fashion-designing/cakestore-main/client/src/pages/home/HomePage.jsx
+++ fashion-designing/cakestore-main/client/src/pages/home/HomePage.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react';
+
+import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { motion, AnimatePresence } from 'framer-motion';
 import { getProducts } from '../../services/productServic';
